045bc3bf596e171debe6b4191070875155d1b7aa
abiramibharanidharan <abiramibharanidharan@gmail.com>
2017-09-19 09:54:44 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['background-color:red'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Emulated works!'
+ 
+
 }
